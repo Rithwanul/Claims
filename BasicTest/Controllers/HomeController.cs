0018cb16b0e35cf30e80e82211b5757e2db1a803
Rithwan <Rithwan@DESKTOP-N1C8I9M>
2020-10-11 00:01:28 +0600

    Claims Complete

---
+++ BasicTest/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BasicTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public IActionResult Secret()
+        {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+
+            var gramdmaClaims = new List<Claim>() 
+            { 
+                new Claim(ClaimTypes.Name, "Dico"),
+                new Claim(ClaimTypes.Email, "rithwanul@gmail.com"),
+                new Claim("Grandma.Says", "I love you!!")
+            };
+
+            var licenceClaim = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, "Bob"),
+                new Claim("DriverSkill", "A+")
+            };
+
+            var grandMaIdentity = new ClaimsIdentity(gramdmaClaims, "Grandma Identity");
+            var licenceIdentity = new ClaimsIdentity(licenceClaim, "Government");
+
+            var userPrinciple = new ClaimsPrincipal(new[] { grandMaIdentity, licenceIdentity });
+
+            HttpContext.SignInAsync(userPrinciple);
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- BasicTest/Startup.cs
+++ BasicTest/Startup.cs
@@ -16,6 +16,13 @@ namespace BasicTest
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("CookieAuth").AddCookie("CookieAuth", config => 
+            {
+                config.Cookie.Name = "Grandmas.Cookie";
+                config.LoginPath = "/Home/Authenticate";
+            });
+
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -28,12 +35,13 @@ namespace BasicTest
 
             app.UseRouting();
 
+            app.UseAuthentication();
+
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapDefaultControllerRoute();
             });
         }
     }
